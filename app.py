d13caef3b06e99fe5fb2a6470e581ead4546fce1
Christopher Ochs <ochscs@mail.uc.edu>
2020-02-22 18:02:34 -0500

    adding stuff

--- app.py
+++ app.py
@@ -1,43 +1,39 @@
 # app.py
 from flask import Flask, request, jsonify
 from iexfinance.stocks import *
-import datetime as dt
+from datetime import datetime as dt
 
 app = Flask(__name__)
+
 
 @app.route('/getstockinfo/', methods=['GET'])
 def respond():
     # Retrieve the name from url parameter
     ticker = request.args.get("Stock", type=str)
-    start_date = request.args.get("Start", type=str)
-    end_date = request.args.get("Start", type=str)
+    raw_start = request.args.get("Start", type=str)
+    raw_end = request.args.get("Start", type=str)
 
-    sl = [int(x) for x in str.split(start_date, "-")]
-    el = [int(x) for x in str.split(end_date, "-")]
+    raw_start = "2019-5-1"
+    raw_end = "2019-5-8"
+    ticker = "TSLA"
 
-    start_date = dt.datetime(sl[0], sl[1], sl[2])
-    end_date = dt.datetime(el[0], el[1], el[2])
+    sl = [int(x) for x in str.split(raw_start, "-")]
+    el = [int(x) for x in str.split(raw_end, "-")]
+
+    start_date = dt(sl[0], sl[1], sl[2])
+    end_date = dt(el[0], el[1], el[2])
 
     data = get_historical_data(ticker, start_date, end_date)
+    vals = []
 
+    for key in data.keys():
+        vals.append(data[key]["close"])
 
-    response = {}
-
-    s = Stock("TSLA")
-
-    dates = [dt.date(2018, 5, 1),
-             dt.date(2018, 5, 2),
-             dt.date(2018, 5, 3),
-             dt.date(2018, 5, 4),
-             dt.date(2018, 5, 5),
-             dt.date(2018, 5, 6),
-             dt.date(2018, 5, 7)]
-
-    response["x"] = dates
+    response = {"x": list(data.keys())}
 
     y = {}
     y["model1"] = [100.34, 95.67, 103.88, 120.55, 75.98, 80.65, 89.13]
-    y["actual"] = [100.00, 80.55, 60.99, 170.88, 120.70, 99.00, 77.77]
+    y["actual"] = vals
 
     response["y"] = y
 

73b484f341d899c6073dd66ec2577fb062ce61f0
Matthew K <krauskmr@mail.uc.edu>
2020-02-22 22:08:39 -0500

    Initial connections established to database

---
+++ cred.py
@@ -0,0 +1,4 @@
+host = "ec2-54-197-34-207.compute-1.amazonaws.com"
+dbase_name = "d6ksbl8386600f"
+user = "ahbzxyieaiqpic"
+password = "9218eb46657ec3bc8368cfb2a34f06ceb5c2fc1ee469ca5858a130cc66d12093"
--- database_functions.py
+++ database_functions.py
@@ -1,37 +1,76 @@
 import psycopg2 as pg2
+from cred import *
+from datetime import date as date
+import hashlib
 
 def get_conn():
-    dbase_name = ""
-    user = ""
-    host = ""
-    password = ""
     try:
-        conn = pg2.connect("dbname='" + dbase_name + "' user='" + user +
-                       "' host='" + host + "' password='" + password + "'")
+        query = "dbname='{}' user='{}' host='{}' password='{}'".format(
+                dbase_name, user, host, password)
+        conn = pg2.connect(query)
     except:
-        pass
+        print("Error! Failed to connect")
         #Handle connection error
     cur = conn.cursor()
-    return cur
+    return cur, conn
+
+def close_conn(cur):
+    cur.close()
 
 def create_table():
-    cur = get_conn()
-    pass
+    cur, conn = get_conn()
+    cur.execute('DROP TABLE IF EXISTS users;')
+    query = """CREATE TABLE users (
+                            Username varchar(255) NOT NULL,
+                            Passwd_Hash varchar(255) NOT NULL,
+                            Email varchar(255) NOT NULL,
+                            last_login DATE NOT NULL,
+                            PRIMARY KEY (Username, Email));"""
+    if not cur.execute(query):
+        print("Success!")
+    else:
+        print("Failure....")
+    conn.commit()
+    close_conn(cur)
 
-def insert_into():
-    cur = get_conn()
-    pass
+def insert_into(user_name, pword, email):
+    cur, conn = get_conn()
+    last_login = date.today()
+    m = hashlib.sha256()
+    m.update(bytes(pword, 'utf8'))
+    pword_hash = m.hexdigest()
+    query = "INSERT INTO users (Username, Passwd_Hash, Email, last_login) VALUES ('{}', '{}', '{}', '{}');".format(user_name, pword_hash, email, last_login)
+    cur.execute(query)
+    conn.commit()
+    close_conn(cur)
 
 def delete_from():
-    cur = get_conn()
+    cur, conn = get_conn()
+    close_conn(cur)
     pass
 
 def select_from(fields, table, conditions):
-    cur = get_conn()
+    cur, conn = get_conn()
     field_str = " "
     for field in fields[:-1]:
         field_str += field + ", "
     field_str += fields[-1]
 
-    query = '''SELECT ''' + field_str + ''' FROM ''' + table
-    cur.execte(query)
+    query = 'SELECT {} FROM {}'.format(field_str, table)
+    if conditions:
+        query += ' WHERE ' + conditions
+    query += ";"
+
+    cur.execute(query)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    close_conn(cur)
+
+def tester():
+    create_table()
+    insert_into("krauskmr", "my_password", "krauskmr@mail.uc.edu")
+    select_from(["last_login"], "users", None)
+
+tester()

e4381e31ee236b9aad4d93b870bcec02bb046918
sakuto3795 <krauskmr@mail.uc.edu>
2020-02-22 23:43:26 -0500

    Added authorization functions

--- database_functions.py
+++ database_functions.py
@@ -7,7 +7,8 @@ import time
 def get_conn():
     # Establish Connection
     # TODO Remove timer
-    tic = time.perf_counter()
+    if DEBUG:
+        tic = time.perf_counter()
     # Try to connect
     try:
         query = "dbname='{}' user='{}' host='{}' password='{}'".format(
@@ -19,8 +20,10 @@ def get_conn():
 
     # Establish cursor
     cur = conn.cursor()
-    toc = time.perf_counter()
-    print(f"Established connection in {toc - tic:0.4f} seconds")
+    # TODO Remove debug code
+    if DEBUG:
+        toc = time.perf_counter()
+        print(f"Established connection in {toc - tic:0.4f} seconds")
     return cur, conn
 
 
@@ -46,10 +49,46 @@ def insert_users(user_name, pword, email):
     last_login = date.today()
     # Hash password
     pword_hash = encode(pword)
-    # Build Query
-    query = "INSERT INTO users (Username, Passwd_Hash, Email, last_login) VALUES ('{}', '{}', '{}', '{}');".format(user_name, pword_hash, email, last_login)
-    # Execute and commit Query
+    # Query to see if username taken
+    query_user = "SELECT 1 FROM users WHERE Username='{}' ;".format(user_name)
+    # Query to see if email is in use
+    query_email = "SELECT 1 FROM users WHERE Email='{}';".format(email)
+    # Exit if username taken
+    if execute(query_email, cur):
+        print("The email "+ email + " is already associated with an account!")
+        cur.close()
+        return
+    # Exit if email is in use
+    elif execute(query_user, cur):
+        print("The username " + user_name + " is already in use!")
+        cur.close()
+        return
+    else:
+        # Build Query
+        query = "INSERT INTO users (Username, Passwd_Hash, Email, last_login) VALUES ('{}', '{}', '{}', '{}');".format(user_name, pword_hash, email, last_login)
+        # Execute and commit query
+        save(query, cur, conn)
+
+def auth_user(user_name, pword):
+    # Get connection
+    cur, conn = get_conn()
+    # Hash password
+    pword_hash = encode(pword)
+    # Returns object if match; [] if no match
+    query = "SELECT 1 FROM users WHERE Username='{}' and Passwd_Hash='{}';".format(user_name, pword_hash)
+    return execute(query, cur, close=True) != []
+
+def change_password(user_name, old_pass, new_pass):
+    # Ensure old password matches
+    if not auth_user(user_name, old_pass):
+        print('Old password incorrect')
+        return
+    # Get connection
+    cur, conn = get_conn()
+    pword_hash = encode(new_pass)
+    query = "UPDATE users SET Passwd_Hash ='{}' WHERE Username='{}';".format(pword_hash, user_name)
     save(query, cur, conn)
+    print("Update successful")
 
 def delete_from():
     cur, conn = get_conn()
@@ -76,13 +115,16 @@ def select_from(fields, table, conditions):
     rows = execute(query, cur)
     for row in rows:
         print(row)
+    cur.close()
 
-
+# TODO modify for final product
 def encode(password):
     # Encode user password
+    if not DEBUG:
+        return password
     m = hashlib.sha256()
     m.update(bytes(password, 'utf8'))
-    pword_hash = m.hexdigest()
+    return m.hexdigest()
 
 def save(query, cur, conn):
     # Execute query, commit, and close connection
@@ -90,16 +132,22 @@ def save(query, cur, conn):
     conn.commit()
     cur.close()
 
-def execute(query, cur):
+def execute(query, cur, close=False):
     # Execute query, return rows, and close connection
     cur.execute(query)
     rows = cur.fetchall()
-    cur.close()
+    if close:
+        cur.close()
     return rows
 
-def tester():
-    create_table()
-    insert_users("krauskmr", "my_password", "krauskmr@mail.uc.edu")
-    select_from(["last_login"], "users", None)
 
-tester()
+def tester():
+    #create_table()
+    #insert_users("krauskmr", "my_password", "krauskmr@mail.uc.edu")
+    #select_from(["*"], "users", None)
+    #print(auth_user("krauskmr", "my_password"))
+    change_password("krauskmr","new_password","old_password")
+
+if __name__ == '__main__':
+    DEBUG = 1
+    tester()

38264c058a3e63cec372406b2cb5afd54e45b4c6
Christopher Ochs <ochscs@mail.uc.edu>
2020-02-22 21:07:18 -0500

    added cors

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # app.py
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from iexfinance.stocks import *
 from datetime import datetime as dt
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/getstockinfo/', methods=['GET'])
 def respond():

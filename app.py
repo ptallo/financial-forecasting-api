ab554268ddcd29b52f8dbfa780d6204cbc0e2f67
Christopher Ochs <ochscs@mail.uc.edu>
2020-02-22 22:42:51 -0500

    fixed Merge Conflicts

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 # app.py
 from flask import Flask, request, jsonify
+import flask_login
+from flask_cors import *
 from iexfinance.stocks import *
 from datetime import datetime as dt
 
 app = Flask(__name__)
+login_manager = flask_login.LoginManager()
+CORS(app, origins='*')
 
 
 @app.route('/getstockinfo/', methods=['GET'])
@@ -35,21 +39,45 @@ def respond():
     return jsonify(response)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = flask_login.LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        flask_login.login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
+
+
 @app.route('/post/', methods=['POST'])
 def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD": "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+
+    # param = request.form.get('name')
+    # print(param)
+    # # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    # if param:
+    #     return jsonify({
+    #         "Message": f"Welcome {name} to our awesome platform!!",
+    #         # Add this option to distinct the POST request
+    #         "METHOD": "POST"
+    #     })
+    # else:
+    #     return jsonify({
+    #         "ERROR": "no name found, please send a name."
+    #     })
 
 
 # A welcome message to test our server

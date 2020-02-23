d30c1bda431914eb0608f94380d5529bf7342a92
Matthew K <krauskmr@mail.uc.edu>
2020-02-22 22:46:32 -0500

    Clean inital draft, modularize

--- database_functions.py
+++ database_functions.py
@@ -2,75 +2,104 @@ import psycopg2 as pg2
 from cred import *
 from datetime import date as date
 import hashlib
+import time
 
 def get_conn():
+    # Establish Connection
+    # TODO Remove timer
+    tic = time.perf_counter()
+    # Try to connect
     try:
         query = "dbname='{}' user='{}' host='{}' password='{}'".format(
                 dbase_name, user, host, password)
         conn = pg2.connect(query)
+    # TODO Handle connection failure
     except:
         print("Error! Failed to connect")
-        #Handle connection error
+
+    # Establish cursor
     cur = conn.cursor()
+    toc = time.perf_counter()
+    print(f"Established connection in {toc - tic:0.4f} seconds")
     return cur, conn
 
-def close_conn(cur):
-    cur.close()
 
 def create_table():
+    # Establish connection
     cur, conn = get_conn()
+    # TODO Remove later
     cur.execute('DROP TABLE IF EXISTS users;')
+    # Create user table
     query = """CREATE TABLE users (
                             Username varchar(255) NOT NULL,
                             Passwd_Hash varchar(255) NOT NULL,
                             Email varchar(255) NOT NULL,
                             last_login DATE NOT NULL,
                             PRIMARY KEY (Username, Email));"""
-    if not cur.execute(query):
-        print("Success!")
-    else:
-        print("Failure....")
-    conn.commit()
-    close_conn(cur)
+    # Execute, commit, and close
+    save(query, cur, conn)
 
-def insert_into(user_name, pword, email):
+def insert_users(user_name, pword, email):
+    # Insert into table Users
     cur, conn = get_conn()
+    # Grab current date
     last_login = date.today()
-    m = hashlib.sha256()
-    m.update(bytes(pword, 'utf8'))
-    pword_hash = m.hexdigest()
+    # Hash password
+    pword_hash = encode(pword)
+    # Build Query
     query = "INSERT INTO users (Username, Passwd_Hash, Email, last_login) VALUES ('{}', '{}', '{}', '{}');".format(user_name, pword_hash, email, last_login)
-    cur.execute(query)
-    conn.commit()
-    close_conn(cur)
+    # Execute and commit Query
+    save(query, cur, conn)
 
 def delete_from():
     cur, conn = get_conn()
-    close_conn(cur)
+    cur.close()
     pass
 
 def select_from(fields, table, conditions):
+    # Get connection 
     cur, conn = get_conn()
+    # Build fields string to select
     field_str = " "
     for field in fields[:-1]:
         field_str += field + ", "
     field_str += fields[-1]
 
+    # Build query
     query = 'SELECT {} FROM {}'.format(field_str, table)
+    # Handle special conditions
     if conditions:
         query += ' WHERE ' + conditions
     query += ";"
 
-    cur.execute(query)
-
-    rows = cur.fetchall()
+    # Execute query
+    rows = execute(query, cur)
     for row in rows:
         print(row)
-    close_conn(cur)
+
+
+def encode(password):
+    # Encode user password
+    m = hashlib.sha256()
+    m.update(bytes(password, 'utf8'))
+    pword_hash = m.hexdigest()
+
+def save(query, cur, conn):
+    # Execute query, commit, and close connection
+    cur.execute(query)
+    conn.commit()
+    cur.close()
+
+def execute(query, cur):
+    # Execute query, return rows, and close connection
+    cur.execute(query)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
 
 def tester():
     create_table()
-    insert_into("krauskmr", "my_password", "krauskmr@mail.uc.edu")
+    insert_users("krauskmr", "my_password", "krauskmr@mail.uc.edu")
     select_from(["last_login"], "users", None)
 
 tester()

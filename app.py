e5e59117d7e835a2af8b1ff48213a15fd1e432d1
sakuto3795 <krauskmr@mail.uc.edu>
2020-02-22 19:01:00 -0500

    Comemnt and refactor respond function

--- app.py
+++ app.py
@@ -13,29 +13,23 @@ def respond():
     raw_start = request.args.get("Start", type=str)
     raw_end = request.args.get("End", type=str)
 
-    raw_start = "2019-5-1"
-    raw_end = "2019-5-8"
-    ticker = "TSLA"
-
+    # List comprehension to make start date
     sl = [int(x) for x in str.split(raw_start, "-")]
+    # List comprehension to make end date
     el = [int(x) for x in str.split(raw_end, "-")]
 
+    # Start date built from sl code comprehension
     start_date = dt(sl[0], sl[1], sl[2])
+    # End date built from el code comprehension
     end_date = dt(el[0], el[1], el[2])
 
+    # Grab data from stock (ticker) from last end_date-start_date days
     data = get_historical_data(ticker, start_date, end_date)
-    vals = []
 
-    for key in data.keys():
-        vals.append(data[key]["close"])
-
-    response = {"x": list(data.keys())}
-
-    y = {}
-    y["model1"] = [100.34, 95.67, 103.88, 120.55, 75.98, 80.65, 89.13]
-    y["actual"] = vals
-
-    response["y"] = y
+    # Extract date keys from historical data
+    vals = [data[key]["close"] for key in data.keys()]
+    # Put Dates and Close values into response
+    response = {"x": list(data.keys()), "y": vals}
 
     # Return the response in json format
     return jsonify(response)

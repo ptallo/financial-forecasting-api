4a3cb3b210879f0ccdac946e09c766093b17f47c
Philip Tallo <tallopt@mail.uc.edu>
2020-03-23 17:46:11 -0400

    clean: wrote tests for all relevant dbo functions

---
+++ test_datatbase_functions.py
@@ -0,0 +1,129 @@
+import psycopg2 as pg2
+from cred import *  # Import login credentials
+import hashlib
+import random
+import string
+
+from database_objects import tools
+from database_objects import users
+from database_objects import favorites
+
+
+testuser_info = {
+    "username": "testuser",
+    "password": "chrisisdumb",
+    "passwordChange": "ma22ispoopoo",
+    "favorite": "AAPL"
+}
+
+
+def run_favorites_dbo_tests(cur, conn):
+    favorites_dbo = favorites.FavoritesTable(cur, conn)
+    user_dbo = users.UsersTable(cur, conn)
+    favorites_dbo_tests = [
+        test_add_remove_favorite
+    ]
+
+    failed_tests = 0
+    for test in favorites_dbo_tests:
+        try:
+            test_add_remove_favorite(user_dbo, favorites_dbo)
+        except AssertionError:
+            failed_tests += 1
+
+    return len(favorites_dbo_tests), failed_tests
+
+
+def test_add_remove_favorite(user_dbo: users.UsersTable, favorites_dbo: favorites.FavoritesTable):
+    if not user_dbo.insert_user(testuser_info.get("username"), testuser_info.get("passwordChange")):
+        raise Exception('test not set up properly')
+
+    assertEqual(favorites_dbo.add_favorite(testuser_info.get(
+        'username'), testuser_info.get('favorite')), True)
+
+    assertEqual(favorites_dbo.remove_favorite(testuser_info.get(
+        'username'), testuser_info.get('favorite')), True)
+
+    if not user_dbo.delete_user(testuser_info.get("username"), testuser_info.get("passwordChange")):
+        raise Exception('test not cleaned up properly')
+
+
+def run_user_dbo_tests(cur, conn):
+    user_dbo = users.UsersTable(cur, conn)
+    user_dbo_tests = [
+        test_insert_delete_user,
+        test_duplicate_insert_user,
+        test_authenticate_user,
+        test_change_password
+    ]
+
+    failed_tests = 0
+    for test in user_dbo_tests:
+        try:
+            test(user_dbo)
+        except AssertionError:
+            failed_tests += 1
+
+    return len(user_dbo_tests), failed_tests
+
+
+def test_duplicate_insert_user(user_dbo):
+    assertEqual(user_dbo.insert_user(testuser_info.get(
+        "username"), testuser_info.get("password")), True)
+    assertEqual(user_dbo.insert_user(testuser_info.get(
+        "username"), testuser_info.get("password")), False)
+    if not user_dbo.delete_user(testuser_info.get("username"), testuser_info.get("password")):
+        raise Exception('test not cleaned up properly')
+
+
+def test_insert_delete_user(user_dbo):
+    assertEqual(user_dbo.insert_user(testuser_info.get(
+        "username"), testuser_info.get("password")), True)
+    assertEqual(user_dbo.delete_user(testuser_info.get(
+        "username"), testuser_info.get("password")), True)
+
+
+def test_authenticate_user(user_dbo):
+    assertEqual(user_dbo.insert_user(testuser_info.get(
+        "username"), testuser_info.get("password")), True)
+    assertEqual(user_dbo.authenticate_user(testuser_info.get(
+        "username"), testuser_info.get("password")), True)
+    if not user_dbo.delete_user(testuser_info.get("username"), testuser_info.get("password")):
+        raise Exception('test not cleaned up properly')
+
+
+def test_change_password(user_dbo):
+    assertEqual(user_dbo.insert_user(testuser_info.get(
+        "username"), testuser_info.get("password")), True)
+    assertEqual(user_dbo.authenticate_user(testuser_info.get(
+        "username"), testuser_info.get("password")), True)
+    assertEqual(user_dbo.change_user_password(
+        testuser_info.get("username"), testuser_info.get("password"), testuser_info.get("passwordChange")), True)
+    assertEqual(user_dbo.authenticate_user(testuser_info.get(
+        "username"), testuser_info.get("passwordChange")), True)
+    if not user_dbo.delete_user(testuser_info.get("username"), testuser_info.get("passwordChange")):
+        raise Exception('test not cleaned up properly')
+
+
+def assertEqual(actual, expected):
+    if actual != expected:
+        raise AssertionError(
+            'actual {} doesn\'t equal expected {}'.format(actual, expected))
+
+
+if __name__ == '__main__':
+    cur, conn = tools.get_conn()
+
+    table_tests = [
+        run_user_dbo_tests,
+        run_favorites_dbo_tests
+    ]
+
+    total, failed = 0, 0
+    for table_test in table_tests:
+        t, f = table_test(cur, conn)
+        total += t
+        failed += f
+    cur.close()
+
+    print('{} tests passed! {} tests failed!'.format(total-failed, failed))

a4a33148903f4be727dd007c15eef0b1b24ea165
Philip Tallo <tallopt@mail.uc.edu>
2020-03-23 22:59:39 -0400

    clean: moved iex handling to its own class and file

---
+++ utils/iex_handler.py
@@ -0,0 +1,16 @@
+
+
+class IEXHandler:
+    def __init__(self):
+        self.base_url = "https://sandbox.iexapis.com"
+        self.api_key = "Tsk_942768a24d244db38c7a44e32e3bc000"
+
+    def get_historical_data(self, ticker, drange="3m"):
+        request_url = "{}/stable/stock/{}/chart/{}?token={}".format(
+            self.base_url,
+            ticker,
+            drange,
+            self.api_key
+        )
+        response = requests.get(request_url)
+        return response.json()

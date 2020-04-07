0321a08f36628069c6768d726be87766c974dfe1
Christopher Ochs <ochscs@mail.uc.edu>
2020-04-07 12:17:38 -0400

    ISSUE 10: Closed
    Added a get_valid_tickers app route and an iex support for creating a (ticker, company name) tuples.

--- utils/iex_handler.py
+++ utils/iex_handler.py
@@ -5,6 +5,8 @@ class IEXHandler:
     def __init__(self):
         self.base_url = "https://sandbox.iexapis.com"
         self.api_key = "Tsk_942768a24d244db38c7a44e32e3bc000"
+        self.stock_tickers = []
+        self.populate_info()
 
     def get_historical_data(self, ticker, drange="3m"):
         request_url = "{}/stable/stock/{}/chart/{}?token={}".format(
@@ -16,4 +18,21 @@ class IEXHandler:
         response = requests.get(request_url)
         if response.ok:
             return response.json()
-        raise Exception("Bad response from historical data code: {} reason: {}".format(response.status_code, response.text))
+        raise Exception("Bad response from get historical data code: {} reason: {}".format(response.status_code, response.text))
+
+    def populate_info(self):
+        if not self.stock_tickers:
+            request_url = "{}/stable/ref-data/symbols?token={}".format(
+                self.base_url,
+                self.api_key
+            )
+
+            response = requests.get(request_url)
+
+            if response.ok:
+                for data in response.json():
+                    self.stock_tickers.append((data["symbol"], data["name"]))
+                return
+
+            raise Exception("Bad response from populate stock info data code: {} reason: {}".format(response.status_code, response.text))
+

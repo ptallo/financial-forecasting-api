765c8b5bee6523cd02aa94d90630a90729d4c696
Christopher Ochs <ochscs@mail.uc.edu>
2020-04-13 20:04:57 -0400

    Forgive me for i have sinned :(
    added env variable IEX_USE_SANDBOX. it must be set.

--- utils/iex_handler.py
+++ utils/iex_handler.py
@@ -1,10 +1,19 @@
 import requests
+import os
 
 
 class IEXHandler:
     def __init__(self):
-        self.base_url = "https://sandbox.iexapis.com"
-        self.api_key = "Tsk_942768a24d244db38c7a44e32e3bc000"
+        if os.getenv("IEX_USE_SANDBOX") == 1:
+            print("USING IEX SANDBOX MODE")
+            self.base_url = "https://sandbox.iexapis.com"
+            self.api_key = "Tsk_942768a24d244db38c7a44e32e3bc000"
+        elif os.getenv("IEX_USE_SANDBOX") == 0:
+            print("USING IEX DEVELOPMENT MODE")
+            self.base_url = "https://cloud.iexapis.com"
+            self.api_key = "pk_8d3e9929b76a499695087d0985f2f374"
+        else:
+            raise Exception("Environment Variable IEX_USE_SANDBOX not set please set it to 0 or 1.")
 
     def get_historical_data(self, ticker, date_range):
         if date_range not in ["1y", "6m", "3m", "1m"]:
